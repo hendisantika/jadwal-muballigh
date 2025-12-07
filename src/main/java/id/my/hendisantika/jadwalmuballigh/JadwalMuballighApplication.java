d718f4ca9d8d3402448a33551613c6e5b7937c0b
hendisantika <hendisantika@yahoo.co.id>
2025-12-07 19:26:48 +0700

    feat: Init project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jadwalmuballigh/JadwalMuballighApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jadwalmuballigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadwalMuballighApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(JadwalMuballighApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/jadwalmuballigh/JadwalMuballighApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jadwalmuballigh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JadwalMuballighApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
